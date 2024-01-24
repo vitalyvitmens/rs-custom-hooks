f1f7f75789f7681391bc18200d4f74bfb7cac864
vitalyvitmens <otm_@tut.by>
2024-01-24 22:53:31 +0300

    chore(src\App.js): коментирует строки с ещё нерешенными задачами

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { Task1 } from './Tasks/Task1'
-import { Task2 } from './Tasks/Task2'
-import { Task3 } from './Tasks/Task3'
-import { Task4 } from './Tasks/Task4'
+// import { Task2 } from './Tasks/Task2'
+// import { Task3 } from './Tasks/Task3'
+// import { Task4 } from './Tasks/Task4'
 import styles from './app.module.css'
 
 export const App = () => {
@@ -10,9 +10,9 @@ export const App = () => {
 			<h1>Custom Hooks</h1>
 			<header className={styles.appHeader}>
 				<Task1 />
-				<Task2 />
-				<Task3 />
-				<Task4 />
+				{/* <Task2 /> */}
+				{/* <Task3 /> */}
+				{/* <Task4 /> */}
 			</header>
 		</div>
 	)
