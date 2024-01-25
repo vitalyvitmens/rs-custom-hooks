9938c2179104582f26715db1065ea57d90288d89
vitalyvitmens <otm_@tut.by>
2024-01-25 17:11:52 +0300

    chore(src\App.js): раскомментирует строки компонента Task2 и его импорта комментирует Task1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import { Task1 } from './Tasks/Task1'
-// import { Task2 } from './Tasks/Task2'
+// import { Task1 } from './Tasks/Task1'
+import { Task2 } from './Tasks/Task2'
 // import { Task3 } from './Tasks/Task3'
 // import { Task4 } from './Tasks/Task4'
 import styles from './app.module.css'
@@ -9,8 +9,8 @@ export const App = () => {
 		<div className={styles.app}>
 			<h1>Custom Hooks</h1>
 			<header className={styles.appHeader}>
-				<Task1 />
-				{/* <Task2 /> */}
+				{/* <Task1 /> */}
+				<Task2 />
 				{/* <Task3 /> */}
 				{/* <Task4 /> */}
 			</header>
