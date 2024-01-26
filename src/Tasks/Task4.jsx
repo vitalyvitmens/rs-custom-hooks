88b44ab9ac5ab7ee8a25899a7a2029fbf37db8a5
vitalyvitmens <otm_@tut.by>
2024-01-26 11:55:11 +0300

    feat(src\Tasks\Task4.jsx): добавляет компонент и переменную view, элементы с классами

--- src/Tasks/Task4.jsx
+++ src/Tasks/Task4.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import { useViewportSize } from '../hooks/useViewportSize'
+import styles from './tasks.module.css'
+import { useMemo } from 'react'
 
-export const Task4 = () => {
-  return (
-    <h4>Task4</h4>
-  )
+export function Task4() {
+	const { height, width } = useViewportSize()
+
+	const view = useMemo(() => {
+		return (
+			<div className={styles.view}>
+				<div className={styles.width}>↔ Width: {width} px</div>
+				<div className={styles.height}>↕ Height: {height} px</div>
+			</div>
+		)
+	}, [height, width])
+
+	return (
+		<div>
+			<h4>Task4</h4>
+			{view}
+		</div>
+	)
 }
