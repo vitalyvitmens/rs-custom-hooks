03455390b44e98d8d49882e76931c0d5d8faf62f
vitalyvitmens <otm_@tut.by>
2024-01-26 16:21:14 +0300

    chore(src\App.js): раскомментирует все компоненты с решением всех задач и их импорты

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-// import { Task1 } from './Tasks/Task1'
-// import { Task2 } from './Tasks/Task2'
-// import { Task3 } from './Tasks/Task3'
-// import { Task4 } from './Tasks/Task4'
-// import { AdditionalTask1 } from './Tasks/AdditionalTask1'
+import { Task1 } from './Tasks/Task1'
+import { Task2 } from './Tasks/Task2'
+import { Task3 } from './Tasks/Task3'
+import { Task4 } from './Tasks/Task4'
+import { AdditionalTask1 } from './Tasks/AdditionalTask1'
 import { AdditionalTask2 } from './Tasks/AdditionalTask2'
 import styles from './app.module.css'
 
@@ -11,11 +11,11 @@ export const App = () => {
 		<div className={styles.app}>
 			<h1>Custom Hooks</h1>
 			<header className={styles.appHeader}>
-				{/* <Task1 /> */}
-				{/* <Task2 /> */}
-				{/* <Task3 /> */}
-				{/* <Task4 /> */}
-				{/* <AdditionalTask1 /> */}
+				<Task1 />
+				<Task2 />
+				<Task3 />
+				<Task4 />
+				<AdditionalTask1 />
 				<AdditionalTask2 />
 			</header>
 		</div>
